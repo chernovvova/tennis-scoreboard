528d8b569e8fde7e35d3e1c7a58ef564775e95a3
Vladimir Chernov <chernovvova2023@yandex.ru>
2024-08-12 01:39:12 +0300

    feat: add CrudRepository interface for database entities

---
+++ src/main/java/ru/chernov/tennisscoreboard/repositories/CrudRepository.java
@@ -0,0 +1,12 @@
+package ru.chernov.tennisscoreboard.repositories;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CrudRepository<T> {
+    Optional<T> findById(Long id);
+    List<T> findAll();
+    void delete(T t);
+    void save(T t);
+    void update(T t);
+}
