c8b51127a46a86c9e6ce774042380ba6fddfdaa2
Vladimir Chernov <chernovvova2023@yandex.ru>
2024-08-12 01:40:32 +0300

    feat: add implementation of CrudRepository for Player entity

---
+++ src/main/java/ru/chernov/tennisscoreboard/repositories/PlayerRepository.java
@@ -0,0 +1,60 @@
+package ru.chernov.tennisscoreboard.repositories;
+
+import org.hibernate.Session;
+import ru.chernov.tennisscoreboard.models.Player;
+import ru.chernov.tennisscoreboard.utils.HibernateUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class PlayerRepository implements CrudRepository<Player> {
+    @Override
+    public Optional<Player> findById(Long id) {
+        Optional<Player> player = Optional.empty();
+        try (Session session = HibernateUtil.getSessionFactory().openSession()){
+            session.beginTransaction();
+            player = Optional.of(session.get(Player.class, id));
+            session.getTransaction().commit();
+        }
+        return player;
+    }
+
+    @Override
+    public List<Player> findAll() {
+        List<Player> players = new ArrayList<>();
+        try (Session session = HibernateUtil.getSessionFactory().openSession()){
+            session.beginTransaction();
+            players = session.createQuery("from Player").list();
+            session.getTransaction().commit();
+        }
+        return players;
+    }
+
+    @Override
+    public void delete(Player player) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()){
+            session.beginTransaction();
+            session.remove(player);
+            session.getTransaction().commit();
+        }
+    }
+
+    @Override
+    public void save(Player player) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()){
+            session.beginTransaction();
+            session.persist(player);
+            session.getTransaction().commit();
+        }
+    }
+
+    @Override
+    public void update(Player player) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()){
+            session.beginTransaction();
+            session.merge(player);
+            session.getTransaction().commit();
+        }
+    }
+}
