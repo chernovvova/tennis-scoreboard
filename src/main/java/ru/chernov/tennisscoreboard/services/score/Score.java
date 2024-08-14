4d0b4c93f5f2c83e347f404604a5df7a45b40900
Vladimir Chernov <chernovvova2023@yandex.ru>
2024-08-15 00:13:42 +0300

    feat: add abstract score model class

---
+++ src/main/java/ru/chernov/tennisscoreboard/services/score/Score.java
@@ -0,0 +1,35 @@
+package ru.chernov.tennisscoreboard.services.score;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Score<T> {
+    private final List<T> score = new ArrayList<>();
+
+    protected abstract T getZeroScore();
+
+    public Score() {
+        score.add(getZeroScore());
+        score.add(getZeroScore());
+    }
+
+    public T getPlayerScore(int playerNumber) {
+        return score.get(playerNumber);
+    }
+
+    public T getOpponentScore(int playerNumber) {
+        int opponentNumber = playerNumber == 0 ? 1 : 0;
+        return score.get(opponentNumber);
+    }
+
+    public void setPlayerScore(T newScore, int playerNumber) {
+        score.set(playerNumber, newScore);
+    }
+
+    public void setOpponentScore(T newScore, int playerNumber) {
+        int opponentNumber = playerNumber == 0 ? 1 : 0;
+        score.set(opponentNumber, newScore);
+    }
+
+    public abstract MatchState pointWonCalculation(int pointWinnerNumber);
+}
