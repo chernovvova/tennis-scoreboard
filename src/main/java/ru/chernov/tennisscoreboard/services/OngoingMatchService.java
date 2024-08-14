789b3152923dc4474d04ac03b4e65917fe01b12f
Vladimir Chernov <chernovvova2023@yandex.ru>
2024-08-14 13:39:41 +0300

    feat: change in memory storage to thread safety structure

--- src/main/java/ru/chernov/tennisscoreboard/services/OngoingMatchService.java
+++ src/main/java/ru/chernov/tennisscoreboard/services/OngoingMatchService.java
@@ -2,12 +2,12 @@ package ru.chernov.tennisscoreboard.services;
 
 import ru.chernov.tennisscoreboard.models.Match;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class OngoingMatchService {
-    private static Map<UUID, Match> matches = new HashMap<>();
+    private static Map<UUID, Match> matches = new ConcurrentHashMap<>();
 
     public Match getMatch(UUID id) {
         return matches.get(id);
