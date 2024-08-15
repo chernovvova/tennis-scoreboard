1e8590e260cc628494d7865e5156b1b3c27a38f4
Vladimir Chernov <chernovvova2023@yandex.ru>
2024-08-16 02:32:25 +0300

    feat: add TestFortyFortySituation test
    the game doesn't end on a goal after forty-forty score

--- src/test/java/services/score/RegularGameScoreTest.java
+++ src/test/java/services/score/RegularGameScoreTest.java
@@ -8,6 +8,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RegularGameScoreTest {
 
+
+    /*first player wins three points and does not win the game,
+    after winning the fourth point the game ends with the result PLAYER_ONE_WON*/
     @Test
     public void TestFourGoalsWin() {
         RegularGameScore regularGameScore = new RegularGameScore();
@@ -17,5 +20,18 @@ public class RegularGameScoreTest {
         }
 
         assertEquals(MatchState.PLAYER_ONE_WON, regularGameScore.winCalculation(0));
+    }
+
+    // the game doesn't end on a goal after forty-forty score
+    @Test
+    public void TestFortyFortySituation() {
+        RegularGameScore regularGameScore = new RegularGameScore();
+
+        for(int i = 0; i < 3; i++) {
+            assertEquals(MatchState.ONGOING, regularGameScore.winCalculation(0));
+            assertEquals(MatchState.ONGOING, regularGameScore.winCalculation(1));
+        }
+
+        assertEquals(MatchState.ONGOING, regularGameScore.winCalculation(0));
     }
 }
