575bbd50b6735796f16fdd37d53ec48459ca36df
Vladimir Chernov <chernovvova2023@yandex.ru>
2024-08-16 01:40:16 +0300

    feat: add getWonStateByPlayerId

--- src/main/java/ru/chernov/tennisscoreboard/services/score/MatchState.java
+++ src/main/java/ru/chernov/tennisscoreboard/services/score/MatchState.java
@@ -3,5 +3,12 @@ package ru.chernov.tennisscoreboard.services.score;
 public enum MatchState {
     ONGOING,
     PLAYER_ONE_WON,
-    PLAYER_TWO_WON
+    PLAYER_TWO_WON;
+
+    public static MatchState getWonStateByPlayerId(int playerId) {
+        if(playerId == 0) {
+            return PLAYER_ONE_WON;
+        }
+        return PLAYER_TWO_WON;
+    }
 }
