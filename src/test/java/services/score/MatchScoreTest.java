0881868f071feb27c7427c10bff276e9aa152b6b
Vladimir Chernov <chernovvova2023@yandex.ru>
2024-08-16 05:47:12 +0300

    feat: add TestTwoOneScoreWin test

--- src/test/java/services/score/MatchScoreTest.java
+++ src/test/java/services/score/MatchScoreTest.java
@@ -18,6 +18,26 @@ public class MatchScoreTest {
         }
 
         assertEquals(MatchState.PLAYER_ONE_WON, matchScore.winCalculation(0));
+    }
 
+    @Test
+    public void TestTwoOneScoreWin() {
+        MatchScore matchScore = new MatchScore(COUNT_SETS_TO_WIN);
+
+        for(int i = 0; i < 24; i++) {
+            matchScore.winCalculation(0);
+        }
+
+        for(int i = 0; i < 23; i++) {
+            matchScore.winCalculation(1);
+        }
+
+        assertEquals(MatchState.ONGOING, matchScore.winCalculation(1));
+
+        for(int i = 0; i < 23; i++) {
+            matchScore.winCalculation(0);
+        }
+
+        assertEquals(MatchState.PLAYER_ONE_WON, matchScore.winCalculation(0));
     }
 }
