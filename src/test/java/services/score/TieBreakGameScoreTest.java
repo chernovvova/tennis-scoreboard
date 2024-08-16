f8c315ec4e57d0d35672754b0a29751aa21f55a9
Vladimir Chernov <chernovvova2023@yandex.ru>
2024-08-16 03:05:14 +0300

    feat: add TestWinOnSevenPoints test

---
+++ src/test/java/services/score/TieBreakGameScoreTest.java
@@ -0,0 +1,23 @@
+package services.score;
+
+import org.junit.jupiter.api.Test;
+import ru.chernov.tennisscoreboard.services.score.MatchState;
+import ru.chernov.tennisscoreboard.services.score.TieBreakGameScore;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TieBreakGameScoreTest {
+
+    @Test
+    public void TestWinOnSevenPoints() {
+        TieBreakGameScore tieBreakGameScore = new TieBreakGameScore();
+
+        for (int i = 0; i < 5; i++) {
+            tieBreakGameScore.winCalculation(0);
+            tieBreakGameScore.winCalculation(1);
+        }
+
+        assertEquals(MatchState.ONGOING, tieBreakGameScore.winCalculation(0));
+        assertEquals(MatchState.PLAYER_ONE_WON, tieBreakGameScore.winCalculation(0));
+    }
+}
