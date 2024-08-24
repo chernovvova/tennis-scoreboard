4a76b7a127f22abe021392d3ffe0659125953b0e
Vladimir Chernov <chernovvova2023@yandex.ru>
2024-08-24 10:40:03 +0300

    refactor: add context path

--- src/main/java/ru/chernov/tennisscoreboard/servlets/NewMatchServlet.java
+++ src/main/java/ru/chernov/tennisscoreboard/servlets/NewMatchServlet.java
@@ -5,13 +5,12 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import ru.chernov.tennisscoreboard.models.Match;
 import ru.chernov.tennisscoreboard.services.MatchGeneratorService;
 
 import java.io.IOException;
 import java.util.UUID;
 
-@WebServlet(name = "NewMatchServlet", value = "/tennis_scoreboard/new_match")
+@WebServlet(name = "NewMatchServlet", value = "/new_match")
 public class NewMatchServlet extends HttpServlet {
     MatchGeneratorService matchGenerator = new MatchGeneratorService();
     @Override
@@ -28,12 +27,14 @@ public class NewMatchServlet extends HttpServlet {
         || player1Name.isEmpty() || player2Name.isEmpty()) {
             resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Wrong parameters");
         }
-        if(player1Name.equals(player2Name)) {
+
+        else if(player1Name.equals(player2Name)) {
             resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Player cannot play with himself");
         }
-
-        UUID newId = matchGenerator.generateMatch(player1Name, player2Name);
-        String url = String.format("/tennis_scoreboard/match-score?uuid=$" + newId.toString());
-        resp.sendRedirect(url);
+        else {
+            UUID newId = matchGenerator.generateMatch(player1Name, player2Name);
+            String url = String.format(req.getContextPath() + "/match-score?uuid=" + newId.toString());
+            resp.sendRedirect(url);
+        }
     }
 }
