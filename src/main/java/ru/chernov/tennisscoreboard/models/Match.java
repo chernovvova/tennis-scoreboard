73da799ef5bd169927e18248429ea9b636661d06
Vladimir Chernov <chernovvova2023@yandex.ru>
2024-08-24 10:37:51 +0300

    feat: add score field

--- src/main/java/ru/chernov/tennisscoreboard/models/Match.java
+++ src/main/java/ru/chernov/tennisscoreboard/models/Match.java
@@ -3,6 +3,8 @@ package ru.chernov.tennisscoreboard.models;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
+import ru.chernov.tennisscoreboard.services.score.MatchScore;
 
 @Entity
 @Table(name = "matches")
@@ -22,9 +24,13 @@ public class Match {
     @JoinColumn(name = "winner", referencedColumnName = "id")
     private Player winner;
 
+    @Transient
+    private MatchScore matchScore;
+
 
     public Match(Player player1, Player player2) {
         this.player1 = player1;
         this.player2 = player2;
+        this.matchScore = new MatchScore(MatchScore.DEFAULT_COUNT_SETS);
     }
 }
