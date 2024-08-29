8affd7f91106c6c278fe1b8c089dcff8236722d2
Vladimir Chernov <chernovvova2023@yandex.ru>
2024-08-30 00:07:29 +0300

    feat: add nullable and unique constraints for name field

--- src/main/java/ru/chernov/tennisscoreboard/models/Player.java
+++ src/main/java/ru/chernov/tennisscoreboard/models/Player.java
@@ -13,7 +13,7 @@ public class Player {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(name = "name")
+    @Column(name = "name", nullable = false, unique = true)
     private String name;
 
     public Player(String name) {
