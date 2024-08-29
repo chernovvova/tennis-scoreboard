8ebe9f16ae698345a4eded96ae3088a2d95d996b
Vladimir Chernov <chernovvova2023@yandex.ru>
2024-08-30 00:06:56 +0300

    feat: add MatchScoreServlet

---
+++ src/main/java/ru/chernov/tennisscoreboard/servlets/MatchScoreServlet.java
@@ -0,0 +1,54 @@
+package ru.chernov.tennisscoreboard.servlets;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import ru.chernov.tennisscoreboard.models.Match;
+import ru.chernov.tennisscoreboard.models.Player;
+import ru.chernov.tennisscoreboard.repositories.MatchRepository;
+import ru.chernov.tennisscoreboard.services.OngoingMatchService;
+import ru.chernov.tennisscoreboard.services.score.MatchState;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@WebServlet(name = "MatchScoreServlet", urlPatterns = "/match-score")
+public class MatchScoreServlet extends HttpServlet {
+    OngoingMatchService ongoingMatchService = new OngoingMatchService();
+    MatchRepository matchRepository = new MatchRepository();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        UUID uuid = UUID.fromString(req.getParameter("uuid"));
+        Match match = ongoingMatchService.getMatch(uuid);
+        Player p = match.getWinner();
+        System.out.println(p);
+        req.setAttribute("match", match);
+        req.getRequestDispatcher("match-score.jsp").forward(req, resp);
+    }
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        UUID uuid = UUID.fromString(req.getParameter("uuid"));
+        Match match = ongoingMatchService.getMatch(uuid);
+
+        boolean isPlayer1Won = req.getParameter("player-1") == null ? false : true;
+        boolean isPlayer2Won = req.getParameter("player-2") == null ? false : true;
+
+        if(isPlayer1Won) {
+            if(match.getMatchScore().winCalculation(0) == MatchState.PLAYER_ONE_WON) {
+                match.setWinner(match.getPlayer1());
+                matchRepository.save(match);
+            }
+        }
+        else {
+            if(match.getMatchScore().winCalculation(1) == MatchState.PLAYER_TWO_WON) {
+                match.setWinner(match.getPlayer2());
+                matchRepository.save(match);
+            }
+        }
+
+        resp.sendRedirect("match-score?uuid=" + uuid);
+    }
+}
