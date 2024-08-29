c102968b98406a7ce4011802b637aaa8cd850f3f
Vladimir Chernov <chernovvova2023@yandex.ru>
2024-08-29 23:58:52 +0300

    feat: add EndMatchServlet class

---
+++ src/main/java/ru/chernov/tennisscoreboard/servlets/EndMatchServlet.java
@@ -0,0 +1,17 @@
+package ru.chernov.tennisscoreboard.servlets;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "EndMatchServlet", urlPatterns = "/end-match")
+public class EndMatchServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
