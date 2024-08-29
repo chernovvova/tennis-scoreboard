8bc50cf60ecd585aab718493ca57aebc5eedb643
Vladimir Chernov <chernovvova2023@yandex.ru>
2024-08-30 00:00:20 +0300

    feat: add example values to the database

--- src/main/java/ru/chernov/tennisscoreboard/utils/HibernateUtil.java
+++ src/main/java/ru/chernov/tennisscoreboard/utils/HibernateUtil.java
@@ -4,9 +4,20 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import ru.chernov.tennisscoreboard.models.Match;
 import ru.chernov.tennisscoreboard.models.Player;
+import ru.chernov.tennisscoreboard.repositories.MatchRepository;
+import ru.chernov.tennisscoreboard.repositories.PlayerRepository;
 
 public class HibernateUtil {
     private static SessionFactory sessionFactory;
+
+    private static final MatchRepository matchRepository = new MatchRepository();
+    private static final PlayerRepository playerRepository = new PlayerRepository();
+
+    static {
+        Player player1 = new Player("К. АЛЬКАРАС");
+        Player player2 = new Player("Ф. КОБОЛЛИ");
+        matchRepository.save(new Match(player1, player2, player1));
+    }
 
     private HibernateUtil() {
     }
