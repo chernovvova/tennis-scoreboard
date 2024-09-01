4e50a02737ebf0d974c1ce1f84c8a388a6f3d60e
Vladimir Chernov <chernovvova2023@yandex.ru>
2024-09-01 17:38:12 +0300

    feat: add end match servlet

--- src/main/java/ru/chernov/tennisscoreboard/servlets/EndMatchServlet.java
+++ src/main/java/ru/chernov/tennisscoreboard/servlets/EndMatchServlet.java
@@ -5,13 +5,18 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import ru.chernov.tennisscoreboard.services.OngoingMatchService;
 
 import java.io.IOException;
+import java.util.UUID;
 
 @WebServlet(name = "EndMatchServlet", urlPatterns = "/end-match")
 public class EndMatchServlet extends HttpServlet {
+    private final OngoingMatchService ongoingMatchService = new OngoingMatchService();
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+        UUID uuid = UUID.fromString(req.getParameter("uuid"));
+        ongoingMatchService.removeMatch(uuid);
+        resp.sendRedirect("index.jsp");
     }
 }
