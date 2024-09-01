9176250ff1259061ab6eb4de9ce7e6a03d9a9c50
Vladimir Chernov <chernovvova2023@yandex.ru>
2024-09-01 17:36:19 +0300

    refactor: change save method return type

--- src/main/java/ru/chernov/tennisscoreboard/repositories/CrudRepository.java
+++ src/main/java/ru/chernov/tennisscoreboard/repositories/CrudRepository.java
@@ -7,6 +7,6 @@ public interface CrudRepository<T> {
     Optional<T> findById(Long id);
     List<T> findAll();
     void delete(T t);
-    void save(T t);
+    T save(T t);
     void update(T t);
 }
